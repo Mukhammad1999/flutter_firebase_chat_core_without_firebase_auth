0b040e7c38fc7e236eba487d4bdeece114f4c454
Rustamkhonov Mukhammad-Rakhimkhon <45013834+Mukhammad1999@users.noreply.github.com>
2024-02-14 00:06:26 +0500

    export issues

--- lib/flutter_firebase_chat_core.dart
+++ lib/flutter_firebase_chat_core.dart
@@ -2,4 +2,5 @@ library flutter_firebase_chat_core;
 
 export 'src/firebase_chat_core.dart';
 export 'src/firebase_chat_core_config.dart';
+export 'src/firebase_chat_core_user.dart';
 export 'src/util.dart';
